96ddc334d137e0532c571cc8d22a6c60124cfbb8
Sebastian Starroske <Sebastian.Starroske@immobilienscout24.de>
2017-02-28 09:41:28 +0100

    introduced a bug

--- src/main/java/testproject/TestProject.java
+++ src/main/java/testproject/TestProject.java
@@ -1,9 +1,12 @@
 package testproject;
 
+import java.math.BigDecimal;
+
 public class TestProject {
 	
 	public static void main(String[] args) {
 		String alternativeName = "Max";
+		BigDecimal youShouldNotDoThis = new BigDecimal(1.5d);
 		if (args.length > 0) {
 			System.out.println("Hello " + String.join(",", args) + "!");
 			System.out.println("How are you?");
