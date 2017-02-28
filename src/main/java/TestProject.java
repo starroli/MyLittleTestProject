8022fd5bd7c55af3f9557f5f5259a688a6488207
Sebastian Starroske <Sebastian.Starroske@immobilienscout24.de>
2017-02-28 08:54:15 +0100

    added a greeting

--- src/main/java/TestProject.java
+++ src/main/java/TestProject.java
@@ -1,6 +1,10 @@
 public class TestProject {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		if (args.length > 0) {
+			System.out.println("Hello " + String.join(",", args) + "!");
+		} else {
+			System.out.println("Hello World!");
+		}
 	}
 }
