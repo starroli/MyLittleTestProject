861106e53d7d55d027fec044c5ba091c25e36d00
Sebastian Starroske <Sebastian.Starroske@immobilienscout24.de>
2017-02-28 09:26:42 +0100

    added an sonar violation

--- src/main/java/testproject/TestProject.java
+++ src/main/java/testproject/TestProject.java
@@ -1,6 +1,9 @@
+package testproject;
+
 public class TestProject {
 	
 	public static void main(String[] args) {
+		String alternativeName = "Max";
 		if (args.length > 0) {
 			System.out.println("Hello " + String.join(",", args) + "!");
 		} else {
